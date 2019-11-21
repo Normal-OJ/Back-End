c254af21626f6a04037360cf9b959df899200558
AlaRduTP <alardutp37259418@gmail.com>
2019-11-22 04:07:03 +0800

    threading send email

--- model/utils/smtp.py
+++ model/utils/smtp.py
@@ -1,10 +1,20 @@
 from smtplib import SMTP_SSL
 from email.message import EmailMessage
 
+import os
+import threading
 
-def send(from_addr, to_addrs, subject, content):
-	with SMTP_SSL('mail.gandi.net') as server:
-		server.login(from_addr, 'pAejTW2rXHQssD8')
+
+SMTP_SERVER = os.environ.get('SMTP_SERVER')
+SMTP_ADMIN = os.environ.get('SMTP_ADMIN')
+SMTP_ADMIN_PASSWORD = os.environ.get('SMTP_PASSWORD')
+SMTP_NOREPLY = os.environ.get('SMTP_NOREPLY')
+SMTP_NOREPLY_PASSWORD = os.environ.get('SMTP_NOREPLY_PASSWORD')
+
+
+def send(from_addr, password, to_addrs, subject, content):
+	with SMTP_SSL(SMTP_SERVER) as server:
+		server.login(from_addr, password)
 		msg = EmailMessage()
 		msg['From'] = from_addr
 		msg['To'] = ', '.join(to_addrs)
@@ -14,4 +24,5 @@ def send(from_addr, to_addrs, subject, content):
 
 
 def send_noreply(to_addrs, subject, content):
-	send('noreply@noj.tw', to_addrs, subject, content)
+	args = (SMTP_NOREPLY, SMTP_NOREPLY_PASSWORD, to_addrs, subject, content)
+	threading.Thread(target=send, args=args).start()
