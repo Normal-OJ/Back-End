84c41025dbeb78bd6b57283ee6514a20f5142a61
冰塊 <aisu170232@gmail.com>
2019-12-06 12:01:02 +0800

    Add files via upload

--- model/profile.py
+++ model/profile.py
@@ -17,34 +17,34 @@ profile_api = Blueprint('profile_api', __name__)
 @profile_api.route('/<id>', methods=['GET'])
 @login_required
 def view_profile(user, id):
-	"""try:
+    """try:
 	
 	except:
 		return HTTPError('Profile not exist.', 404)"""
-	data = {
-		'username': user.username,
-		'email': user.obj.email,
-		'displayed_name': user.obj.profile.displayed_name,
-		'bio': user.obj.profile.bio
-	}
-	return HTTPResponse('Profile exist.', data=data)
+    data = {
+        'username': user.username,
+        'email': user.obj.email,
+        'displayed_name': user.obj.profile.displayed_name,
+        'bio': user.obj.profile.bio
+    }
+    return HTTPResponse('Profile exist.', data=data)
 
 
 @profile_api.route('', methods=['POST'])
 @login_required
 @Request.json(['displayedName', 'bio'])
 def edit_profile(user, displayedName, bio):
-	try:
-		if displayedName != "":
-			user.obj.update(profile={
-				'displayed_name': displayedName,
-				'bio': user.obj.profile.bio
-			})
-		if bio != "":
-			user.obj.update(profile={
-				'displayed_name': user.obj.profile.displayed_name,
-				'bio': bio
-			})
-	except:
-		return HTTPError('Upload fail.', 403)
-	return HTTPResponse('Uploaded.')
+    try:
+        if displayedName != "":
+            user.obj.update(profile={
+                'displayed_name': displayedName,
+                'bio': user.obj.profile.bio
+            })
+        if bio != "":
+            user.obj.update(profile={
+                'displayed_name': user.obj.profile.displayed_name,
+                'bio': bio
+            })
+    except:
+        return HTTPError('Upload fail.', 403)
+    return HTTPResponse('Uploaded.')
