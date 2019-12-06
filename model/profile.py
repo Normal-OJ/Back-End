5add3abdc714535c824ad1e4b4a0401d657a5cfd
冰塊 <aisu170232@gmail.com>
2019-12-06 19:56:57 +0800

    Add files via upload

--- model/profile.py
+++ model/profile.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from mongo import User
 
@@ -14,37 +14,54 @@ JWT_SECRET = os.environ.get('JWT_SECRET')
 profile_api = Blueprint('profile_api', __name__)
 
 
-@profile_api.route('/<id>', methods=['GET'])
+@profile_api.route('/<username>', methods=['GET'])
 @login_required
-def view_profile(user, id):
-    """try:
-	
-	except:
-		return HTTPError('Profile not exist.', 404)"""
-    data = {
-        'username': user.username,
-        'email': user.obj.email,
-        'displayed_name': user.obj.profile.displayed_name,
-        'bio': user.obj.profile.bio
-    }
+def view_others_profile(user, username):
+    try:
+        user = User(username)
+        data = {
+            'username': user.username,
+            'email': user.obj.email,
+            'displayed_name': user.obj.profile.displayed_name,
+            'bio': user.obj.profile.bio
+        }
+    except:
+        return HTTPError('Profile not exist.', 404)
+
     return HTTPResponse('Profile exist.', data=data)
 
 
-@profile_api.route('', methods=['POST'])
+@profile_api.route('', methods=['GET', 'POST'])
 @login_required
-@Request.json(['displayedName', 'bio'])
-def edit_profile(user, displayedName, bio):
-    try:
-        if displayedName != "":
-            user.obj.update(profile={
-                'displayed_name': displayedName,
-                'bio': user.obj.profile.bio
-            })
-        if bio != "":
-            user.obj.update(profile={
+def view_or_edit_profile(user):
+    @Request.json(['displayedName', 'bio'])
+    def edit_profile(displayedName, bio):
+        try:
+            profile = user.obj.profile
+
+            if displayedName != None:
+                profile['displayed_name'] = displayedName
+            if bio != None:
+                profile['bio'] = bio
+
+            user.obj.update(profile=profile)
+        except:
+            return HTTPError('Upload fail.', 403)
+
+        return HTTPResponse('Uploaded.')
+
+    if request.method == 'GET':
+        try:
+            data = {
+                'username': user.username,
+                'email': user.obj.email,
                 'displayed_name': user.obj.profile.displayed_name,
-                'bio': bio
-            })
-    except:
-        return HTTPError('Upload fail.', 403)
-    return HTTPResponse('Uploaded.')
+                'bio': user.obj.profile.bio
+            }
+        except:
+            return HTTPError('Profile not exist.', 404)
+
+        return HTTPResponse('Profile exist.', data=data)
+
+    else:
+        return view_or_edit_profile()
