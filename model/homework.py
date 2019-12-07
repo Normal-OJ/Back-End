b5b83e65394f8eb6279dcb6d671b5c73f2919cc9
hank <hack870629@gmail.com>
2019-12-07 15:59:48 +0800

    db 欄位修改以及再homework api新增驗證user role的func

--- model/homework.py
+++ model/homework.py
@@ -1,12 +1,17 @@
 from flask import Blueprint
+import jwt
 from .auth import login_required
-from mongo import HomeWork
+from mongo import HomeWork,User
 from .utils import HTTPResponse, HTTPRedirect, HTTPError, Request
+import os
 
+JWT_ISS = os.environ.get('JWT_ISS')
+JWT_SECRET = os.environ.get('JWT_SECRET')
 hw_api = Blueprint('homework_api', __name__)
 
 @hw_api.route('/admin/course/<id>/homework/post',methods=['POST'])
 @login_required
+@identity_verify
 @Request.json(['name', 'start', 'end','problemIds','participants'])
 def add_hw(id,name,start,end,problemIds,participants):
     try:
@@ -17,6 +22,7 @@ def add_hw(id,name,start,end,problemIds,participants):
 
 @hw_api.route('/admin/course/<id>/homework/update',methods=['UPDATE'])
 @login_required
+@identity_verify
 @Request.json(['name', 'start', 'end','problemIds','participants'])
 def update_hw_in_course(id,name,start,end,problemIds,participants):
     try:
@@ -27,10 +33,23 @@ def update_hw_in_course(id,name,start,end,problemIds,participants):
 
 @hw_api.route('/admin/course/<id>/homework/delete',methods=['DELETE'])
 @login_required
+@identity_verify
 @Request.json(['name', 'start', 'end','problemIds','participants'])
-def add_hw(id,name,start,end,problemIds,participants):
+def delete_hw(id,name,start,end,problemIds,participants):
     try:
-        homework = HomeWork.update_hw_in_course(id,name,start,end,problemIds);
+        homework = HomeWork.delete_hw_in_course(id,name,start,end,problemIds);
     except Exception as ex:
         return HTTPError(ex,500)    
     return HTTPResponse('Delete homework Success',200,'ok',homework)
+
+def identity_verify(func):
+    #use to verify user role , if user role is not admin return 403
+
+    @Request.cookies(vars_dict={'token': 'jwt'})
+    def wrapper(token, *args, **kwargs):
+            json = jwt.decode(token, JWT_SECRET, issuer=JWT_ISS)
+            user = User.objects(username = json['data']['username']).first()
+            if(user.role!=0):
+                return HTTPError('Inactive User', 403)
+            return func(*args, **kwargs)
+    return  wrapper
