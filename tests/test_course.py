e6e2ac59233ae7b903d8f509a61b75acd5f8ba3d
skps2010 <skps2010030908@yahoo.com>
2019-12-10 15:58:10 +0800

    fix style

--- tests/test_course.py
+++ tests/test_course.py
@@ -4,7 +4,7 @@ import pytest
 class TestAdminCourse:
     '''Test courses panel used my admins
     '''
-    def test_signup_and_login(self, client,test_token):
+    def test_signup_and_login(self, client, test_token):
         '''client.post('/auth/signup',
                             json={
                                 'username': 'test',
@@ -21,15 +21,14 @@ class TestAdminCourse:
                                 'agreement': True
                             })'''
         rv = client.post('/auth/session',
-                            json={
-                                'username': 'test',
-                                'password': 'test'
-                            })
+                         json={
+                             'username': 'test',
+                             'password': 'test'
+                         })
         json = rv.get_json()
         assert rv.status_code == 200
         assert json['status'] == 'ok'
         assert json['message'] == 'Login Success'
-
 
     def test_view(self, client):
         # Get all courses
@@ -40,27 +39,21 @@ class TestAdminCourse:
         assert json['message'] == 'Success.'
         assert json['data'] == []
 
-
     def test_add_with_invalid_username(self, client):
         # add a course with non-existent username
         rv = client.post('/course',
-                            json={
-                                'course': 'math',
-                                'teacher': 'testt'
-                            })
+                         json={
+                             'course': 'math',
+                             'teacher': 'testt'
+                         })
         json = rv.get_json()
         assert rv.status_code == 404
         assert json['status'] == 'err'
         assert json['message'] == '"User not found."'
 
-
     def test_add(self, client):
         # add a course
-        rv = client.post('/course',
-                            json={
-                                'course': 'math',
-                                'teacher': 'test'
-                            })
+        rv = client.post('/course', json={'course': 'math', 'teacher': 'test'})
         json = rv.get_json()
         assert rv.status_code == 200
         assert json['status'] == 'ok'
@@ -68,7 +61,4 @@ class TestAdminCourse:
 
         rv = client.get('/course')
         json = rv.get_json()
-        assert json['data'] == [{
-                'course': 'math',
-                'teacher': 'test'
-            }]
+        assert json['data'] == [{'course': 'math', 'teacher': 'test'}]
