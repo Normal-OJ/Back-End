7c40bf2e786daf1f43e009fe3578ae9b78d939d1
冰塊 <aisu170232@gmail.com>
2019-12-17 11:29:23 +0800

    Update and use yapf3

--- mongo/engine.py
+++ mongo/engine.py
@@ -10,44 +10,36 @@ connect('normal-oj', host=MONGO_HOST)
 
 
 class Profile(EmbeddedDocument):
-    displayed_name = StringField(db_field='displayedName',
-                                 required=True,
-                                 max_length=16)
+    displayed_name = StringField(
+        db_field='displayedName', required=True, max_length=16)
     bio = StringField(max_length=64, required=True, default='')
 
 
 class EditorConfig(EmbeddedDocument):
-    font_size = IntField(db_field='fontSize',
-                         min_value=8,
-                         max_value=72,
-                         default=14)
-    theme = StringField(default='default',
-                        choices=[
-                            "default", "base16-dark", "base16-light",
-                            "dracula", "eclipse", "material", "monokai"
-                        ])
+    font_size = IntField(
+        db_field='fontSize', min_value=8, max_value=72, default=14)
+    theme = StringField(
+        default='default',
+        choices=[
+            "default", "base16-dark", "base16-light", "dracula", "eclipse",
+            "material", "monokai"
+        ])
     indent_type = IntField(db_field='indentType', default=1, choices=[0, 1])
-    tab_size = IntField(db_field='tabSize',
-                        default=4,
-                        min_value=1,
-                        max_value=8)
+    tab_size = IntField(
+        db_field='tabSize', default=4, min_value=1, max_value=8)
     language = IntField(default=0, choices=[0, 1, 2])
 
 
 class User(Document):
-    user_id = StringField(db_field='userId',
-                          max_length=24,
-                          required=True,
-                          unique=True)
+    user_id = StringField(
+        db_field='userId', max_length=24, required=True, unique=True)
     username = StringField(max_length=16, required=True, unique=True)
     email = EmailField(required=True, unique=True)
     active = BooleanField(default=False)
     role = IntField(default=2, choices=[0, 1, 2])
     profile = EmbeddedDocumentField(Profile, default=Profile, null=True)
-    editor_config = EmbeddedDocumentField(EditorConfig,
-                                          db_field='editorConfig',
-                                          default=EditorConfig,
-                                          null=True)
+    editor_config = EmbeddedDocumentField(
+        EditorConfig, db_field='editorConfig', default=EditorConfig, null=True)
     # contest_id = ReferenceField('Contest', db_field='contestId')
     course_ids = ListField(ReferenceField('Course'), db_field='courseIds')
     # submission_ids = ListField(ReferenceField('Submission'), db_field='submissionIds')
@@ -55,10 +47,8 @@ class User(Document):
 
 class Course(Document):
     course_status = IntField(default=0, choices=[0, 1])
-    course_name = StringField(max_length=64,
-                              required=True,
-                              unique=True,
-                              db_field='courseName')
+    course_name = StringField(
+        max_length=64, required=True, unique=True, db_field='courseName')
     teacher = ReferenceField('User', db_field='teacher')
     tas = ListField(ReferenceField('User'), db_field='tas')
     student_nicknames = DictField(db_field='studentNicknames')
@@ -67,6 +57,7 @@ class Course(Document):
     # announcement_ids = ListField(ReferenceField('Announcement'), db_field='announcementIds')
     # post_ids = ListField(ReferenceField('Post'), db_field='postIds')
 
+
 class Case(EmbeddedDocument):
     input = StringField()
     output = StringField()
@@ -74,25 +65,25 @@ class Case(EmbeddedDocument):
     memory_limit = IntField(db_field='memoryLimit')
     time_limit = IntField(db_field='timeLimit')
 
+
 class TestCase(EmbeddedDocument):
     language = IntField(choices=[1, 2, 4])
-    fill_in_template = StringField(db_field='fillInTemplate',
-                                   max_length=16000)
+    fill_in_template = StringField(db_field='fillInTemplate', max_length=16000)
     cases = ListField(ReferenceField('Case'))
 
+
 class Problem(Document):
+    problem_id = StringField(db_field='problemId', required=True, unique=True)
     course_ids = ListField(ReferenceField('Course'), db_field='courseIds')
     problem_status = IntField(default=1, choices=[0, 1])
     problem_type = IntField(default=0, choices=[0, 1])
-    problem_name = StringField(db_field='problemName',
-                               max_length=64,
-                               required=True,
-                               unique=True)
+    problem_name = StringField(
+        db_field='problemName', max_length=64, required=True, unique=True)
     markdown = StringField(max_length=100000, required=True)
     owner = StringField(max_length=16, required=True)
-    # pdf = 
-    test_case = EmbeddedDocumentField(TestCase,
-                                      db_field='testCase',
-                                      default=TestCase,
-                                      null=True)
-    
+    # pdf =
+    tags = ListField(StringField(max_length=16))
+    test_case = EmbeddedDocumentField(
+        TestCase, db_field='testCase', default=TestCase, null=True)
+    ac_user = IntField(db_field='ACUser', default=0)
+    submitter = IntField(default=0)
