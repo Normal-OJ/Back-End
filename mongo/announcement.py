045130a03b80ad48e32ec3ebc78624998f3ca5b6
Ikaros1110 <40647020s+Ikaros1110@users.noreply.github.com>
2019-12-20 05:09:17 +0800

    complete add

--- mongo/announcement.py
+++ mongo/announcement.py
@@ -14,6 +14,19 @@ class Announcement:
             return "Announcement not found"
         return target
 
+    def add_announcement(user,course,title,content):# course=course_id
+        target = User(user).obj
+        if target is None:
+            return "User not found."
+        created_time = timestamp_now() # local time utc+8
+        updated_time = created_time
+        engine.Announcement(announcement_name=title,
+                            course_id=course,
+                            author=user,
+                            created=created_time,
+                            updated=updated_time,
+                            markdown=content)
+
     def edit_announcement(course,title,content):
         courseExist = get_obj(engine.Course, course_name=course)
         if courseExist == None:
