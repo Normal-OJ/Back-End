57b96337a371b0ac42f1274452e34c2e53fba197
冰塊 <aisu170232@gmail.com>
2019-12-20 02:11:54 +0800

    Add files via upload

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,16 @@
-from .auth import auth_api
-from .test import test_api
-from .profile import profile_api
-from .course import course_api
+from . import auth
+from . import profile
+from . import course
+from . import problem
+from . import test
+
+from .auth import *
+from .profile import *
+from .course import *
+from .problem import *
+from .test import *
+
+__all__ = [
+    *auth.__all__, *profile.__all__, *course.__all__, *problem.__all__,
+    *test.__all__
+]
