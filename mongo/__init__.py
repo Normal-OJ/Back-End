a2e7707285349e461bd8e18955f3fd36016c8bd8
Lu, Jau-Hua <37984865+skps2010@users.noreply.github.com>
2019-12-19 15:36:30 +0800

    Add Public Course (#14)
    * add mongoengine db field
    
    * add api and model
    
    * solve conflict
    
    * add some field in homework
    
    * db 欄位修改以及再homework api新增驗證user role的func
    
    * 修改homework的db欄位
    
    * 更改DB欄位(homework&course)，以及完成所有api
    
    * use yapf format code
    
    * 改回原本的，不加locahost
    
    * 新增exception的處理以及命名歸責問題
    
    * add some exception filter and modify update logic
    
    * delete unused code and space line
    
    * use yapf to format
    
    * change db connect
    
    * 判斷user是否為該課程老師
    
    * temp
    
    * rename some method and check user's identity when create/update/delete hw
    
    * 拿掉500的exception
    
    * use yapf format
    
    * 更新未改到的comment
    
    * 補充 nameerror的錯誤訊息
    
    * change POST to PUT
    
    * add Public course
    
    * fix style

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -14,3 +14,16 @@ __all__ = [
     *course.__all__, *engine.__all__, *user.__all__, *submission.__all__,
     *homework.__all__
 ]
+
+if User("first_admin").obj is None:
+    ADMIN = {
+        'username': 'first_admin',
+        'password': 'firstpasswordforadmin',
+        'email': 'i.am.first.admin@noj.tw'
+    }
+
+    admin = User.signup(**ADMIN)
+    admin.update(active=True, role=0)
+
+if Course("Public").obj is None:
+    add_course("Public", "first_admin")
