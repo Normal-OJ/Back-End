1d686c21b85949fddba17352e4609fd53cb29f61
冰塊 <aisu170232@gmail.com>
2019-12-20 02:12:44 +0800

    Add files via upload

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -1,2 +1,11 @@
-from mongoengine.errors import *
-from .user import User
+from . import course
+from . import problem
+from . import engine
+from . import user
+
+from .course import *
+from .problem import *
+from .engine import *
+from .user import *
+
+__all__ = [*course.__all__, *problem.__all__, *engine.__all__, *user.__all__]
