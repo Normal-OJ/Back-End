157557a1f18ef27d54037e3ca7e224e139dee970
skps2010 <skps2010030908@yahoo.com>
2019-12-22 06:26:55 +0800

    finish unit test

--- tests/test_inbox.py
+++ tests/test_inbox.py
@@ -5,164 +5,125 @@ from tests.base_tester import BaseTester
 class TestInbox(BaseTester):
     '''Test inbox
     '''
-
     def test_send_with_invalid_username(self, client_student):
         # send inbox with all invalide user
         rv = client_student.post('/inbox',
                                  json={
-                                     'receivers': ['nobody'], 'title': 'hi', 'message': 'AAA'
+                                     'receivers': ['nobody'],
+                                     'title': 'hi',
+                                     'message': 'AAA'
                                  })
         json = rv.get_json()
         assert json['message'] == 'At least one receiver is required'
         assert rv.status_code == 400
-        assert json['status'] == 'err'
 
     def test_send_with_invalid_info(self, client_student):
         # send inbox with wierd info
         rv = client_student.post('/inbox',
                                  json={
-                                     'receivers': ['teacher'], 'title': {}, 'message': 'AAA'
+                                     'receivers': ['teacher'],
+                                     'title': {},
+                                     'message': 'AAA'
                                  })
         json = rv.get_json()
         assert json['message'] == 'Failed to Send a Message'
         assert rv.status_code == 400
-        assert json['status'] == 'err'
 
     def test_send(self, client_student):
         # send inbox
         rv = client_student.post('/inbox',
                                  json={
-                                     'receivers': ['teacher'], 'title': 'hi', 'message': 'AAA'
+                                     'receivers': ['teacher'],
+                                     'title': 'hi',
+                                     'message': 'AAA'
                                  })
         json = rv.get_json()
         assert json['message'] == 'Successfully Send'
         assert rv.status_code == 200
-        assert json['status'] == 'ok'
 
-    def test_view(self, client_admin):
-        # Get all courses
-        rv = client_admin.get('/inbox')
+    def test_view(self, client_teacher):
+        # view inbox
+        rv = client_teacher.get('/inbox')
         json = rv.get_json()
         assert json['message'] == 'Received List'
         assert rv.status_code == 200
-        assert json['status'] == 'ok'
-        assert json['data'] == [{'course': 'math', 'teacher': 'admin'}]
+        assert json['data'][0]['message'] == 'aaa'
+        assert json['data'][0]['title'] == 'hi'
+        assert json['data'][0]['sender'] == ['student']
+        assert json['data'][0]['status'] == 0
 
-    def test_add(self, client_admin):
-        # add a course
-        rv = client_admin.post('/course',
-                               json={
-                                   'course': 'math',
-                                   'teacher': 'admin'
-                               })
-        json = rv.get_json()
-        assert json['message'] == 'Success.'
-        assert rv.status_code == 200
-        assert json['status'] == 'ok'
+        message_id = json['data'][0]['messageId']
 
-        rv = client_admin.get('/course')
+    def test_read_without_owner(self, client_student):
+        # read a inbox message when you are not the owner
+        rv = client_student.put('/inbox', json={'messageId': message_id})
         json = rv.get_json()
-        assert json['data'] == [{'course': 'math', 'teacher': 'admin'}]
-
-    def test_add_with_existent_course_name(self, client_admin):
-        # add a course with existent name
-        rv = client_admin.post('/course',
-                               json={
-                                   'course': 'math',
-                                   'teacher': 'admin'
-                               })
-        json = rv.get_json()
-        assert json['message'] == 'Course exists.'
-        assert rv.status_code == 400
-        assert json['status'] == 'err'
-
-    def test_view(self, client_admin):
-        # Get all courses
-        rv = client_admin.get('/course')
-        json = rv.get_json()
-        assert json['message'] == 'Success.'
-        assert rv.status_code == 200
-        assert json['status'] == 'ok'
-        assert json['data'] == [{'course': 'math', 'teacher': 'admin'}]
-
-    def test_view_with_non_member(self, client_student):
-        # Get all courses with a user that is not a member
-        rv = client_student.get('/course')
-        json = rv.get_json()
-        assert json['message'] == 'Success.'
-        assert rv.status_code == 200
-        assert json['status'] == 'ok'
-        assert json['data'] == []
-
-    def test_edit_with_invalid_course_name(self, client_admin):
-        # edit a course with non-existent course
-        rv = client_admin.put('/course',
-                              json={
-                                  'course': 'history',
-                                  'newCourse': 'PE',
-                                  'teacher': 'admin'
-                              })
-        json = rv.get_json()
-        assert json['message'] == 'Course not found.'
-        assert rv.status_code == 404
-        assert json['status'] == 'err'
-
-    def test_edit_with_invalid_username(self, client_admin):
-        # edit a course with non-existent username
-        rv = client_admin.put('/course',
-                              json={
-                                  'course': 'math',
-                                  'newCourse': 'PE',
-                                  'teacher': 'adminn'
-                              })
-        json = rv.get_json()
-        assert json['message'] == 'User not found.'
-        assert rv.status_code == 404
-        assert json['status'] == 'err'
-
-    def test_edit(self, client_admin):
-        # edit a course
-        rv = client_admin.put('/course',
-                              json={
-                                  'course': 'math',
-                                  'newCourse': 'PE',
-                                  'teacher': 'admin'
-                              })
-        json = rv.get_json()
-        assert json['message'] == 'Success.'
-        assert rv.status_code == 200
-        assert json['status'] == 'ok'
-
-        rv = client_admin.get('/course')
-        json = rv.get_json()
-        assert json['data'] == [{'course': 'PE', 'teacher': 'admin'}]
-
-    def test_delete_with_invalid_course_name(self, client_admin):
-        # delete a course with non-existent course name
-        rv = client_admin.delete('/course', json={'course': 'math'})
-        json = rv.get_json()
-        assert json['message'] == 'Course not found.'
-        assert rv.status_code == 404
-        assert json['status'] == 'err'
-
-    def test_delete_with_non_owner(self, client_teacher):
-        # delete a course with a user that is not the owner nor an admin
-        rv = client_teacher.delete('/course', json={'course': 'PE'})
-        json = rv.get_json()
-        assert json['message'] == 'Forbidden.'
+        assert json['message'] == 'Failed to Read the Message'
         assert rv.status_code == 403
-        assert json['status'] == 'err'
 
-    def test_delete(self, client_admin):
-        # delete a course
-        rv = client_admin.delete('/course', json={
-            'course': 'PE',
-        })
+    def test_read(self, client_student):
+        # read a inbox message
+        rv = client_student.put('/inbox', json={'messageId': message_id})
         json = rv.get_json()
-        assert json['message'] == 'Success.'
+        assert json['message'] == 'Message Status Changed'
         assert rv.status_code == 200
-        assert json['status'] == 'ok'
 
-        rv = client_admin.get('/course')
+        rv = client_teacher.get('/inbox')
         json = rv.get_json()
-        assert json['data'] == []
+        assert json['data'][0]['status'] == 1
+
+    def test_delete_without_owner(self, client_student):
+        # delete a inbox message when you are not the owner
+        rv = client_student.delete('/inbox', json={'messageId': message_id})
+        json = rv.get_json()
+        assert json['message'] == 'Failed to Access the Message'
+        assert rv.status_code == 403
+
+    def test_delete(self, client_student):
+        # delete a inbox message
+        rv = client_student.delete('/inbox', json={'messageId': message_id})
+        json = rv.get_json()
+        assert json['message'] == 'Message is Deleted'
+        assert rv.status_code == 200
+
+        rv = client_teacher.get('/inbox')
+        json = rv.get_json()
+        assert len(json['data']) == 0
+
+    def test_view_sent(self, client_student):
+        # view sent message
+        rv = client_student.get('/inbox/sent')
+        json = rv.get_json()
+        assert json['message'] == 'Sent List'
+        assert rv.status_code == 200
+        assert json['data'][0]['message'] == 'aaa'
+        assert json['data'][0]['title'] == 'hi'
+        assert json['data'][0]['receivers'] == ['teacher']
+
+    def test_delete_sent_with_invalid_id(self, client_student):
+        # delete a none-exist inbox message
+        rv = client_student.delete('/inbox/sent',
+                                   json={'messageId': message_id})
+        json = rv.get_json()
+        assert json['message'] == 'Message Not Found'
+        assert rv.status_code == 404
+
+    def test_delete_sent_without_owner(self, client_student):
+        # delete a inbox message when you are not the owner
+        rv = client_student.delete('/inbox/sent',
+                                   json={'messageId': message_id})
+        json = rv.get_json()
+        assert json['message'] == 'Failed to Access the Message'
+        assert rv.status_code == 403
+
+    def test_delete_sent(self, client_student):
+        # delete a inbox message
+        rv = client_student.delete('/inbox/sent',
+                                   json={'messageId': message_id})
+        json = rv.get_json()
+        assert json['message'] == 'Deleted'
+        assert rv.status_code == 200
+
+        rv = client_student.get('/inbox/sent')
+        json = rv.get_json()
+        assert len(json['data']) == 0
