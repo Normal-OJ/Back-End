138c5c74804c2bdbeee758cb9e29694e50bdfce2
skps2010 <skps2010030908@yahoo.com>
2019-12-22 03:31:15 +0800

    fix import

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -1,14 +1,22 @@
 from . import course
-from . import problem
 from . import engine
 from . import user
+from . import submission
+from . import homework
+from . import inbox
+from . import problem
 
 from .course import *
-from .problem import *
 from .engine import *
 from .user import *
+from .submission import *
+from .homework import *
+from .inbox import *
+from .problem import *
 
-__all__ = [*course.__all__, *problem.__all__, *engine.__all__, *user.__all__]
+__all__ = [
+    *course.__all__, *engine.__all__, *user.__all__, *submission.__all__,
+    *homework.__all__, *inbox.__all__, *problem.__all__]
 
 if User("first_admin").obj is None:
     ADMIN = {
