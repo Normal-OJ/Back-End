8de6b6d924d6cfa087dc07de675da74f7a2541a2
Ikaros1110 <40647020s+Ikaros1110@users.noreply.github.com>
2019-12-21 23:39:39 +0800

    rename get() to found() & edit add()
    In add_announcement
    save
    course_id = target_course.id -> target_course
    author = user.username -> user
    author have bug

--- mongo/announcement.py
+++ mongo/announcement.py
@@ -1,37 +1,37 @@
 from mongo import engine
 from mongo.course import *
 from datetime import datetime
-from .user import User
+from .user import *
 from .utils import *
-__all__ = ['Announcement','add_announcement','edit_announcement','delete_announcement']
+__all__ = ['Announcement','found_announcement','add_announcement','edit_announcement','delete_announcement']
 
 class Announcement:
     @staticmethod
     def get_all_announcement():
         return engine.announcement.object
-    @staticmethod
-    def get_announcement(course):
-        try:
-            target_course = engine.Course.objects.get(course_name=course)
-        except:
-            raise FileNotFoundError
-        target = Announcement.objects(course_id=target_course.id)
-        if target == None:
-            raise FileNotFoundError
-        return target
+
+def found_announcement(course):
+    try:
+        target_course = engine.Course.objects.get(course_name=course)
+    except:
+        raise FileNotFoundError
+    try:
+        target = engine.Announcement.objects.get(course_id=target_course.id)
+    except:
+        raise FileNotFoundError
+    return target
 
 def add_announcement(user,course,title,content):# course=course_id
     try:
         target_course = engine.Course.objects.get(course_name=course)
     except:
         return "Course not found."
-    course_id=target_course.id
     created_time = datetime.now() # local time use utc+8
     created_time.timestamp()
     updated_time = created_time
     new_announcement = engine.Announcement(announcement_name=title,
-                        course_id=course_id,
-                        author=user.username,
+                        course_id=target_course,
+                        author=user,
                         created=created_time,
                         updated=updated_time,
                         markdown=content)
