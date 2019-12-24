e57b550cf4b5954d40818dc68fba281455e4c68d
aisu170232 <aisu170232@gmail.com>
2019-12-25 02:24:44 +0800

    Finish UnitTest of profile & use yapf

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -14,7 +14,7 @@ class TestViewProfile(BaseTester):
         assert json['status'] == 'ok'
         assert json['message'] == 'Uploaded.'
 
-    def test_view_own_profile(self, client_student):
+    def test_view_profile_without_username(self, client_student):
         rv = client_student.get('/profile')
         json = rv.get_json()
         assert rv.status_code == 200
@@ -22,9 +22,33 @@ class TestViewProfile(BaseTester):
         assert json['message'] == 'Profile exist.'
         assert json['data']['displayedName'] == 'aisu_0911'
         assert json['data']['bio'] == 'Hello World!'
-        '''
-        assert json['data'] == [{
-            'displayedName': 'aisu_0911',
-            'bio': 'Hello World!'
-        }]
-        '''
+
+    def test_view_profile_with_username(self, client_student):
+        rv = client_student.get('/profile/student')
+        json = rv.get_json()
+        assert rv.status_code == 200
+        assert json['status'] == 'ok'
+        assert json['message'] == 'Profile exist.'
+        assert json['data']['displayedName'] == 'aisu_0911'
+        assert json['data']['bio'] == 'Hello World!'
+
+    def test_view_profile_with_nonexist_username(self, client_student):
+        rv = client_student.get('/profile/a_username_not_exist')
+        json = rv.get_json()
+        assert rv.status_code == 404
+        assert json['status'] == 'err'
+        assert json['message'] == 'Profile not exist.'
+
+    def test_invalid_config(self, client_student):
+        rv = client_student.put('/profile/config',
+                                json={
+                                    'fontSize': 87,
+                                    'theme': 'default',
+                                    'indentType': 1,
+                                    'tabSize': 4,
+                                    'language': 0
+                                })
+        json = rv.get_json()
+        assert rv.status_code == 400
+        assert json['status'] == 'err'
+        assert json['message'] == 'Update fail.'
