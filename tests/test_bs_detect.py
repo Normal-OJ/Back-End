6c6db632e6b8b71bc21960aa3361acd7b9ea3775
asef18766 <asef18766@gmail.com>
2019-12-28 21:11:04 +0800

    fix coding style

--- tests/test_bs_detect.py
+++ tests/test_bs_detect.py
@@ -14,15 +14,18 @@ def test_pylint_success():
     status, responese_str = bs.detect(route, "pylint", 3)
     assert status == True
 
+
 def test_cppcheck_failed():
     route = "./tests/bs_detect_sample/dirty_c_code.c"
     status, responese_str = bs.detect(route, "cpp_checkers", 3)
     assert status == False
 
+
 def test_cppcheck_success():
     route = "./tests/bs_detect_sample/good_coding_style.cpp"
     status, responese_str = bs.detect(route, "pylint", 3)
     assert status == True
+
 
 def test_set_settings():
     route = "./tests/bs_detect_sample/one_line_a_plus_b.py"
