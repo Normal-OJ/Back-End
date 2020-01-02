a58ea75024a9eba7ca41e8ce782569de08cf8fb4
bogay <pojay11523@gmail.com>
2020-01-03 04:30:42 +0800

    fix unmerged file

--- mongo/submission.py
+++ mongo/submission.py
@@ -1,10 +1,6 @@
 import json
-<<<<<<< HEAD
 import os
 import pathlib
-=======
-from mongoengine import NotUniqueError
->>>>>>> origin
 from datetime import date
 from typing import List
 from model.submission_config import SubmissionConfig
@@ -19,23 +15,12 @@ __all__ = ['Submission']
 
 class Submission(MongoBase, engine=engine.Submission):
     def __init__(self, submission_id):
-<<<<<<< HEAD
         self.submission_id = str(submission_id)
-=======
-        self.submission_id = submission_id
-
-    def __getattr__(self, name):
-        # convert mongo ObjectId to hex string for serialize
-        if name == 'id':
-            return str(self.obj.id)
-        return super().__getattr__(name)
->>>>>>> origin
 
     def __str__(self):
         return f'submission [{self.submission_id}]'
 
     @property
-<<<<<<< HEAD
     def id(self):
         '''
         convert mongo ObjectId to hex string for serialize
@@ -71,29 +56,6 @@ class Submission(MongoBase, engine=engine.Submission):
 
         with open(path) as f:
             ret = f.read()
-=======
-    def to_py_obj(self):
-        if not self:
-            return {}
-
-        old_keys = [
-            '_id',
-            # 'problem',
-        ]
-        new_keys = [
-            'submissionId',
-            # 'problemId',
-        ]
-        ret = json.loads(self.obj.to_json())
-        for old, new in zip(old_keys, new_keys):
-            ret[new] = ret[old]['$oid']
-            del ret[old]
-
-        ret['timestamp'] = ret['timestamp']['$date'] // 1000
-        ret['problemId'] = engine.Problem.objects.get(
-            id=ret['problem']['$oid']).problem_id
-        del ret['problem']
->>>>>>> origin
 
         return ret
 
