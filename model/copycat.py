cf9eb9d1fdc57ea10d6e8cb0593276cddbcc4583
Siao, Yu-Jie <a6194315535@gmail.com>
2020-02-04 22:47:11 +0800

    copycat add via standford moss

--- model/copycat.py
+++ model/copycat.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request
 from mongo import *
 
+import mosspy
 
 __all__ = ['copycat_api']
 
 copycat_api = Blueprint('copycat_api', __name__)
+
 
 @course_api.route('/', methods=['POST'])
 @login_required
@@ -15,13 +17,42 @@ def detect(user, course):
     if permission < 2:
         return HTTPError('Forbidden.', 403)
 
-    submissions = Submission.filter(user=user, offset=0, count=-1, status=0, problem=problem_id)
-    
-    last_submission = {}
-    for submission in submissions:
-        if submission.user.username not in last_submission:
-            last_submission[submission.user.username] = Submission(submission.id).get_code()
-    
-    for user, code in last_submission.items():
-        
+    # select all ac code
+    submissions = Submission.filter(user=user,
+                                    offset=0,
+                                    count=-1,
+                                    status=0,
+                                    problem=problem_id)
 
+    last_cc_submission = {}
+    last_python_submission = {}
+    for submission in submissions:
+        s = Submission(submission.id)
+        if s.language in [0, 1] and s.user.username not in last_cc_submission:
+            last_cc_submission[submission.user.username] = s.main_code_path
+        elif s.language in [2] \
+            and s.user.username not in last_python_submission:
+            last_python_submission[submission.user.username] = s.main_code_path
+
+    moss_user_id = 97089070
+
+    # check for c or cpp code
+    m1 = mosspy.Moss(userid, "cc")
+
+    for user, code_path in last_cc_submission.items():
+        m1.addFile(code_path)
+    cc_report_url = m1.send()
+
+    # check for python code
+    m2 = mosspy.Moss(userid, "python")
+
+    for user, code_path in last_python_submission.items():
+        m2.addFile(code_path)
+    python_report_url = m2.send()
+
+    # return c & cpp or python report
+    return HTTPResponse('Success.',
+                        data={
+                            "ccReport": cc_report_url,
+                            "pythonReport": python_report_url
+                        })
