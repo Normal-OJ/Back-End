e4d3ce131ddefafe92288f98af92bffbc6650598
Siao, Yu-Jie <a6194315535@gmail.com>
2020-02-05 21:22:08 +0800

    uncomplete copycat test

---
+++ tests/test_copycat.py
@@ -0,0 +1,20 @@
+import pytest
+from tests.base_tester import BaseTester
+
+S_NAMES = {
+    'student': 'Chika.Fujiwara',
+    'student-2': 'Nico.Kurosawa',
+}
+
+
+class TestCopyCat(BaseTester):
+    # user, course, problem, submission
+    # def course():
+    #     make_course(
+    #         username="teacher",
+    #         students=S_NAMES,
+    #     )
+
+    # def prob(problem_ids):
+    #     pids = problem_ids("teacher", 1, True)[0]
+    pass
