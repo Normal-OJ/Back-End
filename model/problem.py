bb8fc6b6f533cb851ff8420e1e680af9527a3266
bogay <pojay11523@gmail.com>
2022-09-22 00:28:18 +0800

    fix: flask's breaking changes

--- model/problem.py
+++ model/problem.py
@@ -236,7 +236,7 @@ def get_testcase(user, problem_id):
         problem.test_case.case_zip,
         mimetype='application/zip',
         as_attachment=True,
-        attachment_filename=f'testdata-{problem_id}.zip',
+        download_name=f'testdata-{problem_id}.zip',
     )
 
 
