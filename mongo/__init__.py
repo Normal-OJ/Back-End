c467469156e9f0b1849a946511e8e7f1f930da06
AlaRduTP <alardutp37259418@gmail.com>
2023-06-07 04:57:40 +0800

    refactor: remove inbox (#235)
    * Remove `/inbox` API and related files.
    
    * Remove `Inbox` mongo document.

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -3,7 +3,6 @@ from . import engine
 from . import user
 from . import submission
 from . import homework
-from . import inbox
 from . import problem
 from . import announcement
 from . import post
@@ -14,7 +13,6 @@ from .engine import *
 from .user import *
 from .submission import *
 from .homework import *
-from .inbox import *
 from .problem import *
 from .announcement import *
 from .post import *
@@ -26,7 +24,6 @@ __all__ = [
     *user.__all__,
     *submission.__all__,
     *homework.__all__,
-    *inbox.__all__,
     *problem.__all__,
     *announcement.__all__,
     *post.__all__,
