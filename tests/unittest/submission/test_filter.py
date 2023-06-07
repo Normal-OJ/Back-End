9cd1efb91e10ea6296fe480b063f1909d4a751aa
bogay <pojay11523@gmail.com>
2023-06-08 03:21:25 +0800

    test(submission): add test cases for filter

--- tests/unittest/submission/test_filter.py
+++ tests/unittest/submission/test_filter.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime, timedelta
 from tests import utils
 from mongo import Submission, User
+import secrets
 
 
 def setup_function(_):
@@ -101,3 +102,66 @@ def test_with_empty_period():
             after=after,
             before=before,
         )
+
+
+def test_invalid_sort_by():
+    admin = utils.user.create_user(role=User.engine.Role.ADMIN)
+    with pytest.raises(
+            ValueError,
+            match=r'.*runTime or memoryUsage.*',
+    ):
+        Submission.filter(user=admin, sort_by='problemIds')
+
+
+def test_query_with_non_exist_problem():
+    admin = utils.user.create_user(role=User.engine.Role.ADMIN)
+    problem_id = utils.problem.create_problem(
+        owner=admin,
+        course='Public',
+    ).problem_id
+    count = 3
+    for _ in range(count):
+        Submission.add(
+            problem_id=problem_id,
+            username=admin.username,
+            lang=0,
+        )
+    results = Submission.filter(user=admin, problem=123456)
+    assert len(results) == 0
+
+
+def test_query_with_non_exist_user():
+    admin = utils.user.create_user(role=User.engine.Role.ADMIN)
+    problem_id = utils.problem.create_problem(
+        owner=admin,
+        course='Public',
+    ).problem_id
+    count = 3
+    for _ in range(count):
+        Submission.add(
+            problem_id=problem_id,
+            username=admin.username,
+            lang=0,
+        )
+    q_user = secrets.token_hex(8)
+    results = Submission.filter(user=admin, q_user=q_user)
+    assert len(results) == 0
+
+
+def test_query_with_lang():
+    admin = utils.user.create_user(role=User.engine.Role.ADMIN)
+    problem_id = utils.problem.create_problem(
+        owner=admin,
+        course='Public',
+    ).problem_id
+    expected_count = 3
+    for lang in range(0, 3):
+        for _ in range(expected_count):
+            Submission.add(
+                problem_id=problem_id,
+                username=admin.username,
+                lang=lang,
+            )
+    for lang in range(0, 3):
+        results = Submission.filter(user=admin, language_type=lang)
+        assert len(results) == expected_count
